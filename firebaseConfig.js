9b58a70781f40f4558009bfd366d7762df6b082e
Yitaris <138614042+yitaris@users.noreply.github.com>
2025-03-20 20:00:09 +0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCBTOk0C46XRVp6HsTCBQlsV-TYBYXwcz8",
-    authDomain: "yitaapp.firebaseapp.com",
-    databaseURL: "https://yitaapp-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "yitaapp",
-    storageBucket: "yitaapp.appspot.com",
-    messagingSenderId: "422420697951",
-    appId: "1:422420697951:web:c4fc912426280b2b871cea"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 const app = initializeApp(firebaseConfig);
